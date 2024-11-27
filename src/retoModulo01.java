6abac61c615daecdc1aaae2e7018763cfb8200f7
Jonatan David Rodriguez <inge.jonatanrodriguez@gmail.com>
2024-11-26 20:30:09 -0500

    Creacion de constantes para naves y la seleccion del usuario

--- src/retoModulo01.java
+++ src/retoModulo01.java
@@ -10,6 +10,17 @@ public class retoModulo01 {
             "Es el planeta mas frio del sistema solar, lleve ruana.",
             "Es el planeta mas lejano del sol, 30 veces mas lejos que la tierra."
     };
+
+    // Constantes para las naves espaciales
+    private static final String[] NAVES = { "Alcerina, reserva de oxigeno 1000 Litros, combustible 10 Galones", "La ruidosa, reserva de oxigeno 3000 Litros, combustible 30 Galones", "Silent Hills, reserva de oxigeno 5000 Litros, combustible 100 Galones" };
+    private static final double[] VELOCIDADES = { 30.0, 50.0, 70.0 }; // Velocidades en millones de km/día
+    
+    // Variables para la selección del usuario
+    private static int planetaSeleccionado = -1;
+    private static int naveSeleccionada = -1;
+    private static int cantidadPasajeros = 1;
+
+    
     public static void main(String[] args) throws Exception {
 
         
