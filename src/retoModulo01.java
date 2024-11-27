c69192a5070c8b0bf929928523a9a99716d4a0f7
Jonatan David Rodriguez <inge.jonatanrodriguez@gmail.com>
2024-11-26 20:28:28 -0500

    Principio de reto

---
+++ src/retoModulo01.java
@@ -0,0 +1,23 @@
+public class retoModulo01 {
+
+
+    private static final String[] PLANETAS = { "Marte", "Júpiter", "Saturno", "Venus", "Neptuno" };
+    private static final double[] DISTANCIAS = { 225.0, 778.0, 1429.0, 556.32, 884.25 }; // Distancias en millones de km
+    private static final String[] DESCRIPCIONES_PLANETAS = {
+            "Es el cuarto planeta del sistema solar.",
+            "Es el planeta más grande del sistema solar.",
+            "Es conocido por sus impresionantes anillos.",
+            "Es el planeta mas frio del sistema solar, lleve ruana.",
+            "Es el planeta mas lejano del sol, 30 veces mas lejos que la tierra."
+    };
+    public static void main(String[] args) throws Exception {
+
+        
+
+        
+
+
+
+        
+    }
+}
